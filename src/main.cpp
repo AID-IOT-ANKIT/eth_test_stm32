4f1d7688a59611adb4fdbb032dd99f0f31ed4acd
AID-IOT-ANKIT <ankit@aid-iot.com>
2024-12-29 08:49:12 -0800

    uart modbus parsing

--- src/main.cpp
+++ src/main.cpp
@@ -1,66 +1,121 @@
+#include <Arduino.h>
 
-#include <LwIP.h>
-#include <STM32Ethernet.h>
+// Define LED pin
+#define LED_PIN PB0
 
-// if you don't want to use DNS (and reduce your sketch size)
-// use the numeric IP instead of the name for the server:
-//IPAddress server(74,125,232,128);  // numeric IP for Google (no DNS)
-char server[] = "www.google.com";    // name address for Google (using DNS)
+// UART parameters
+#define BAUDRATE 38400
+#define SLAVE_ID 0x01 // Slave ID for this device
 
-// Set the static IP address to use if the DHCP fails to assign
-IPAddress ip(192, 168, 0, 177);
+// Buffer to store incoming Modbus frame
+uint8_t modbusBuffer[256];
+uint16_t bufferIndex = 0;
 
-// Initialize the Ethernet client library
-// with the IP address and port of the server
-// that you want to connect to (port 80 is default for HTTP):
-EthernetClient client;
+// Modbus Coil Register
+bool coilState = false;
 
-void setup() {
-  // Open serial communications and wait for port to open:
-  Serial.begin(9600);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
+HardwareSerial Serial1(PG9, PG14); // D0, D1 of stm32f767zi
+
+// CRC Calculation Functions
+uint16_t modbusCRC16(const uint8_t *data, uint16_t length)
+{
+  uint16_t crc = 0xFFFF;
+
+  for (uint16_t i = 0; i < length; i++)
+  {
+    crc ^= data[i];
+    for (uint8_t j = 0; j < 8; j++)
+    {
+      if (crc & 0x0001)
+      {
+        crc = (crc >> 1) ^ 0xA001;
+      }
+      else
+      {
+        crc = crc >> 1;
+      }
+    }
   }
-
-  // start the Ethernet connection:
-  if (Ethernet.begin() == 0) {
-    Serial.println("Failed to configure Ethernet using DHCP");
-    // try to configure using IP address instead of DHCP:
-    Ethernet.begin(ip);
-  }
-  // give the Ethernet shield a second to initialize:
-  delay(1000);
-  Serial.println("connecting...");
-
-  // if you get a connection, report back via serial:
-  if (client.connect(server, 80)) {
-    Serial.println("connected");
-    // Make a HTTP request:
-    client.println("GET /search?q=arduino HTTP/1.1");
-    client.println("Host: www.google.com");
-    client.println("Connection: close");
-    client.println();
-  } else {
-    // if you didn't get a connection to the server:
-    Serial.println("connection failed");
-  }
+  return crc;
 }
 
-void loop() {
-  // if there are incoming bytes available
-  // from the server, read them and print them:
-  if (client.available()) {
-    char c = client.read();
-    Serial.print(c);
+// Function to parse Modbus frame
+void processModbusFrame()
+{
+  // Check if the frame is at least 8 bytes (minimum Modbus RTU frame length)
+  if (bufferIndex < 8)
+  {
+    return;
   }
 
-  // if the server's disconnected, stop the client:
-  if (!client.connected()) {
-    Serial.println();
-    Serial.println("disconnecting.");
-    client.stop();
+  // Check Slave ID
+  if (modbusBuffer[0] != SLAVE_ID)
+  {
+    return; // Not for this slave
+  }
 
-    // do nothing forevermore:
-    while (true);
+  // Validate CRC
+  uint16_t receivedCRC = (modbusBuffer[bufferIndex - 1] << 8) | modbusBuffer[bufferIndex - 2];
+  uint16_t calculatedCRC = modbusCRC16(modbusBuffer, bufferIndex - 2);
+
+  Serial.print("received crc: ");
+  Serial.println(receivedCRC);
+
+  Serial.print("calc crc: ");
+  Serial.println(calculatedCRC);
+
+  if (receivedCRC != calculatedCRC)
+  {
+    return; // CRC mismatch
+  }
+
+  // Process Modbus Function Code
+  uint8_t functionCode = modbusBuffer[1];
+  if (functionCode == 0x05)
+  { // Write Single Coil
+    uint16_t coilAddress = (modbusBuffer[2] << 8) | modbusBuffer[3];
+    uint16_t coilValue = (modbusBuffer[4] << 8) | modbusBuffer[5];
+
+    Serial.print("coil addr: ");
+    Serial.println(coilAddress);
+
+    Serial.print("coil value: ");
+    Serial.println(coilValue);
+
+    if (coilAddress == 0x0000)
+    {                                    // Coil 0 (LED control)
+      coilState = (coilValue == 0xFF00); // Set coil state
+      digitalWrite(LED_PIN, coilState);
+    }
+  }
+
+  // Clear buffer after processing
+  bufferIndex = 0;
+}
+
+void setup()
+{
+  // Initialize UART
+  Serial.begin(115200);
+  Serial1.begin(BAUDRATE, SERIAL_8E1);
+
+  // Initialize LED pin
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, LOW);
+}
+
+void loop()
+{
+  // Read data from UART
+  while (Serial1.available())
+  {
+    modbusBuffer[bufferIndex++] = Serial1.read();
+
+    // Process frame if buffer is full or complete frame received
+    if (bufferIndex >= sizeof(modbusBuffer) || (bufferIndex > 7 && Serial1.peek() == -1))
+    {
+      Serial.println("processing buffer");
+      processModbusFrame();
+    }
   }
 }
